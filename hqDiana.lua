c01392ecfe7f8ee37ba0f2e07afccfb33bb4047c
hq-af <38526394+hq-af@users.noreply.github.com>
2021-06-07 19:16:37 +0200

    1.0.2 added lane/jungle clear

--- hqDiana.lua
+++ hqDiana.lua
@@ -634,7 +634,7 @@ function Diana.Farm()
     if position and count and count > 0 then
       if count and count == 1 then
         local orbTarget = API.Orbwalker.GetTarget()
-        if orbTarget and orbTarget.IsMonster and and orbTarget.DistanceSqr(API.Player) < Diana.Q.Range*Diana.Q.Range then
+        if orbTarget and orbTarget.IsMonster and orbTarget.DistanceSqr(API.Player) < Diana.Q.Range*Diana.Q.Range then
           position = orbTarget:FastPrediction(250 + API.Game.GetLatency())
         end
       end
